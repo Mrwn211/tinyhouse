412116bdb01478e4be6ea0e3f3aef8a730afed27
Merwan Guerrib <merwanguerrib@gmail.com>
2020-11-24 17:59:40 +0100

    Update src/index.ts with the new graphQl typeDefs and resolvers

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,12 @@
 import express from "express";
-import { schema } from './graphql';
-
-import {ApolloServer} from "apollo-server-express";
+import { ApolloServer } from "apollo-server-express";
+import { typeDefs, resolvers } from "./graphql";
 
 const app = express();
 const port = 9000;
 
-const server = new ApolloServer({schema});
-server.applyMiddleware({app, path: "/api"})
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: "/api" });
 
-
-
- app.listen(port);
+app.listen(port);
 console.log(`[app] : http://localhost:${port}`);
